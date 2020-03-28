60970a8e234f585dbe0d1537c42ad58cabed8063
Kalle <qalle85@gmail.com>
2020-03-28 10:27:19 +0200

    Create nesgenie-test.bat

---
+++ nesgenie-test.bat
@@ -0,0 +1,11 @@
+@cls
+python nesgenie.py sxiopo
+python nesgenie.py sxsopo
+python nesgenie.py yeuzugaa
+python nesgenie.py yelzugaa
+python nesgenie.py 91d9:ad
+python nesgenie.py 11d9:ad
+python nesgenie.py acb3?00:07
+python nesgenie.py 2cb3?00:07
+python nesgenie.py cccccc
+python nesgenie.py
