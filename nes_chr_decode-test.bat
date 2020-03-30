69ac171f2e7df92b1a104e468b9803c44174b2f8
Kalle <qalle85@gmail.com>
2020-03-31 02:54:37 +0300

    Update nes_chr_decode-test.bat

--- nes_chr_decode-test.bat
+++ nes_chr_decode-test.bat
@@ -13,6 +13,7 @@ echo === Decoding raw CHR data files ===
 python nes_chr_decode.py test-in\smb1.chr test-out\smb1-chr2.png
 python nes_chr_decode.py --palette 0000ff ff0000 ff00ff 00ff00 test-in\smb1.chr test-out\smb1-chr3.png
 python nes_chr_decode.py test-in\blastermaster.chr test-out\blastermaster-chr2.png
+python nes_chr_decode.py test-in\chr-2color.chr test-out\chr-2color.png
 echo.
 
 echo === Verify the decoded files yourself ===
