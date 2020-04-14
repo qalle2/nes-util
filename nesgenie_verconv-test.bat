6658a3e9a73951792ce2ec909fa056f7b43442ca
Kalle <qalle85@gmail.com>
2020-04-14 15:10:43 +0300

    Update nesgenie_verconv-test.bat

--- nesgenie_verconv-test.bat
+++ nesgenie_verconv-test.bat
@@ -1,7 +1,7 @@
 @echo off
 cls
 
-echo === Journey to Silius: US code XTUSKTAV to EUR (should find XTVSNTAV, but won't before partial matches are implemented) ===
+echo === Journey to Silius: US code XTUSKTAV to EUR (should find XTVSNTAV) ===
 python nesgenie_verconv.py xtusktav test-in\journey.nes test-in\journey-e.nes
 echo.
 
@@ -13,8 +13,16 @@ echo === Journey to Silius: EUR code TESSXTIA to US (should find TEXINTIA) ===
 python nesgenie_verconv.py tessxtia test-in\journey-e.nes test-in\journey.nes
 echo.
 
+echo === Journey to Silius: EUR code TESSXTIA to US, slice len 8 (should find TEXINTIA) ===
+python nesgenie_verconv.py --slice-length 8 tessxtia test-in\journey-e.nes test-in\journey.nes
+echo.
+
+echo === Journey to Silius: EUR code TESSXTIA to US, max diff 2 (should find TEXINTIA) ===
+python nesgenie_verconv.py --max-different-bytes 2 tessxtia test-in\journey-e.nes test-in\journey.nes
+echo.
+
 echo === Help text ===
-python nesgenie_verconv.py
+python nesgenie_verconv.py --help
 echo.
 
 echo === These should cause errors ===
