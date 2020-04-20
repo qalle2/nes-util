bc591b074e9c6e7f8cd5c3b570767b00d9b2cdf0
Kalle <qalle85@gmail.com>
2020-04-21 00:54:53 +0300

    Update nesgenie_6to8.py

--- nesgenie_6to8.py
+++ nesgenie_6to8.py
@@ -4,11 +4,12 @@ import sys
 import ineslib
 import nesgenielib
 
-def get_compare_values(handle, PRGSize, PRGBankSize, address):
+def get_compare_values(handle, PRGSize, PRGBankSize, CPUAddress):
     """Get possible compare values from the PRG ROM. Yield one value per call."""
 
     # get offset within each bank by ignoring the most significant bits of the address
-    offset = address & (PRGBankSize - 1)
+    offset = CPUAddress & (PRGBankSize - 1)
+
     # for each bank, read the byte at that offset
     for PRGPos in range(offset, PRGSize, PRGBankSize):
         handle.seek(16 + PRGPos)
@@ -25,38 +26,37 @@ def main():
         )
     (file, code) = (sys.argv[1], sys.argv[2])  # pylint complains about [1:]
 
-    # validate and decode the code
+    # validate code; get CPU address and replace value
     decoded = nesgenielib.decode_code(code)
     if decoded is None or len(decoded) == 3:
         sys.exit("Invalid six-letter Game Genie code.")
-    (address, replacement) = decoded
+    (CPUAddr, repl) = decoded
 
     try:
         with open(file, "rb") as handle:
+            # read iNES header
             try:
                 fileInfo = ineslib.parse_iNES_header(handle)
             except ineslib.iNESError as e:
                 sys.exit("Error: " + str(e))
+
+            # get PRG ROM bank size
             PRGBankSize = ineslib.get_smallest_PRG_bank_size(fileInfo["mapper"])
             if PRGBankSize >= fileInfo["PRGSize"]:
                 sys.exit("There is no reason to use eight-letter codes with this game.")
 
-            compareValues = set(get_compare_values(
-                handle, fileInfo["PRGSize"], PRGBankSize, address
-            ))
+            # get possible compare values
+            compValues = set(
+                get_compare_values(handle, fileInfo["PRGSize"], PRGBankSize, CPUAddr)
+            )
     except OSError:
         sys.exit("Error reading the file.")
 
-    # ignore a compare value that equals the replacement value
-    compareValues.discard(replacement)
+    # ignore a compare value that equals the replace value
+    compValues.discard(repl)
 
-    if compareValues:
-        print("Try each of these eight-letter codes instead of your six-letter code:")
-        codes = (nesgenielib.encode_code(address, replacement, comp) for comp in compareValues)
-        for code in sorted(codes):
-            print(code)
-    else:
-        print("No eight-letter codes found.")
+    for code in sorted(nesgenielib.encode_code(CPUAddr, repl, comp) for comp in compValues):
+        print(code)
 
 if __name__ == "__main__":
     main()
