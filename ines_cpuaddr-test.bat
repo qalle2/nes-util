c36b178ba26f6e834a89ed2380477b0e8e040fb3
Kalle <qalle85@gmail.com>
2020-05-22 13:09:30 +0300

    Update ines_cpuaddr-test.bat

--- ines_cpuaddr-test.bat
+++ ines_cpuaddr-test.bat
@@ -1,5 +1,5 @@
 @cls
-py nesgenie_cpuaddr.py test-in\blastermaster.nes 1ffff
-py nesgenie_cpuaddr.py test-in\smb1.nes 7fff
-py nesgenie_cpuaddr.py test-in\smb1.nes 8000
-py nesgenie_cpuaddr.py test-in\smb3.nes 3ffff
+py ines_cpuaddr.py test-in\blastermaster.nes 1ffff
+py ines_cpuaddr.py test-in\smb1.nes 7fff
+py ines_cpuaddr.py test-in\smb1.nes 8000
+py ines_cpuaddr.py test-in\smb3.nes 3ffff
