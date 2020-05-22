009d9c1663beed3cd2a8749ffcaf7ad712b1a270
Kalle <qalle85@gmail.com>
2020-05-22 12:51:28 +0300

    Create nesgenie_cpuaddr-test.bat

---
+++ nesgenie_cpuaddr-test.bat
@@ -0,0 +1,5 @@
+@cls
+py nesgenie_cpuaddr.py test-in\blastermaster.nes 1ffff
+py nesgenie_cpuaddr.py test-in\smb1.nes 7fff
+py nesgenie_cpuaddr.py test-in\smb1.nes 8000
+py nesgenie_cpuaddr.py test-in\smb3.nes 3ffff
