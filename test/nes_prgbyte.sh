73a260fa12079f66b8307a53725f53f8beca5305
Kalle <qalle85@gmail.com>
2024-05-09 23:52:44 +0300

    update test

--- test/nes_prgbyte.sh
+++ test/nes_prgbyte.sh
@@ -1,10 +1,11 @@
 clear
 
-echo "=== SMB 1 (0000, 7fff) ==="
+echo "=== SMB 1 (should print 0x78, 0xff) ==="
 python3 ../nes_prgbyte.py ../test-in/smb1.nes 0000
 python3 ../nes_prgbyte.py ../test-in/smb1.nes 7fff
 echo
 
-echo "=== This should cause an error ==="
+echo "=== These should cause two errors ==="
+python3 ../nes_prgbyte.py ../test-in/smb1.nes -1
 python3 ../nes_prgbyte.py ../test-in/smb1.nes 8000
 echo
